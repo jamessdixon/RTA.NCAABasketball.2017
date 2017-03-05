a8c63363aa5bab4d9c8d23ea8c925b72068564a8
Jamie Dixon <jamessdixon@gmail.com>
2017-03-04 22:52:10 -0500

    added flag for regional capacity

--- Basketball.Solution/Predictor/Script.fsx
+++ Basketball.Solution/Predictor/Script.fsx
@@ -30,9 +30,6 @@ let getTeamSeq t =
     [1 .. t.SeedWeight]
     |> Seq.map(fun i -> t)
 
-let testTeam = {Id = "Foo3"; Region="Foo"; Seed=3; SeedWeight=10}
-getTeamSeq testTeam
-
 type TeamPrediction = {Team: Team; Place: string}
 
 let teamPredictions = List<TeamPrediction>()
@@ -52,114 +49,85 @@ let teamAvailable t =
     |> Seq.exists(fun tp -> tp.Team.Id = t.Id)
     |> not
 
-let getPool maxSeed = 
+let regionAvailable t round =
+    let maxNumber =
+        match round with
+        | 1 -> 8 //first win
+        | 2 -> 4 //sweet sixteen
+        | 3 -> 2 //elite 8
+        | 4 -> 1 //final 4
+        | _ -> 1 //final game
+
+    let regionCount = 
+        teamPredictions
+        |> Seq.filter(fun tp -> tp.Team.Region = t.Region)
+        |> Seq.length
+
+    maxNumber > regionCount
+    
+
+let getPool maxSeed round = 
     teams
     |> Seq.filter(fun t -> t.Seed <= maxSeed)
     |> Seq.filter(fun t -> teamAvailable t)
+    |> Seq.filter(fun t -> regionAvailable t round)
     |> Seq.collect(fun t -> getTeamSeq t)
     |> Seq.toArray
     |> shuffle
     
-
-let foo = getPool 8
-let bar = 
-    foo 
-    |> Seq.groupBy(fun p -> p.Seed)
-    |> Seq.map(fun (s, ta) -> s, ta |> Seq.length)
-
-bar |> Seq.iter(fun t -> printfn "%A:%A" (fst t) (snd t))
-
 let placeTeam poolSize placeDesc =
-    let pool = getPool poolSize
+    let round = 
+        match placeDesc with
+        | "Champ" -> 6
+        | "RunnerUp" -> 5
+        | "FinalFour" -> 4
+        | "Elite 8" -> 3
+        | "SweeetSixteen" -> 2
+        | "FirstWin" -> 1
+        | _ -> 0
+
+    let pool = getPool poolSize round
     let index = random.Next(pool |> Seq.length)
     teamPredictions.Add({Team = pool.[index]; Place=placeDesc})
 
+//Need 1 with 6 wins from 1 or 2 seed - champ
+//Need 1 more with 5 wins from 1,2,5,6 seed - runner up
+//Need 2 more with 4 wins from 1,2,5,6 seed - final 4
+//Need 4 more with 3 wins from 1-6 seeds - elite 8
+//Need 8 more with 2 wins from 1-12 seeds - sweet sixteen
+//Need 16 with 1 win from 1-12 seeds - first v
+//Need 32 with 0 wins from 5-16 seeds
+placeTeam 2 "Champ"
+placeTeam 2 "RunnerUp"
+placeTeam 6 "FinalFour"
+placeTeam 6 "FinalFour"
+placeTeam 6 "Elite 8"
+placeTeam 6 "Elite 8"
+placeTeam 6 "Elite 8"
+placeTeam 6 "Elite 8"
+placeTeam 12 "SweeetSixteen"
+placeTeam 12 "SweeetSixteen"
+placeTeam 12 "SweeetSixteen"
+placeTeam 12 "SweeetSixteen"
+placeTeam 12 "SweeetSixteen"
+placeTeam 12 "SweeetSixteen"
+placeTeam 12 "SweeetSixteen"
+placeTeam 12 "SweeetSixteen"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
+placeTeam 12 "FirstWin"
 
-let createMadness = 
-    //Madness
-    //Need 1 with 6 wins from 1-8 seed
-    //Need 2 more with 4 wins from 1-11 seed
-    //Need 4 more with 3 wins from 1-12 seeds
-    //Need 8 more with 2 wins from 1-15 seeds
-    //Need 16 with 1 win from 1-16 seeds
-    //Need 32 with 0 wins from 1-16 seeds
-    placeTeam 8 "Champ"
-    placeTeam 8 "RunnerUp"
-    placeTeam 11 "FinalFour"
-    placeTeam 11 "FinalFour"
-    placeTeam 12 "Elite 8"
-    placeTeam 12 "Elite 8"
-    placeTeam 12 "Elite 8"
-    placeTeam 12 "Elite 8"
-    placeTeam 15 "SweeetSixteen"
-    placeTeam 15 "SweeetSixteen"
-    placeTeam 15 "SweeetSixteen"
-    placeTeam 15 "SweeetSixteen"
-    placeTeam 15 "SweeetSixteen"
-    placeTeam 15 "SweeetSixteen"
-    placeTeam 15 "SweeetSixteen"
-    placeTeam 15 "SweeetSixteen"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-    placeTeam 16 "FirstWin"
-
-    teamPredictions 
-    |> Seq.iter(fun tp -> printfn "%s:%s" tp.Team.Id tp.Place)
-
-let createReality = 
-    //Normal
-    //Need 1 with 6 wins from 1 or 2 seed - champ
-    //Need 1 more with 5 wins from 1,2,5,6 seed - runner up
-    //Need 2 more with 4 wins from 1,2,5,6 seed - final 4
-    //Need 4 more with 3 wins from 1-6 seeds - elite 8
-    //Need 8 more with 2 wins from 1-12 seeds - sweet sixteen
-    //Need 16 with 1 win from 1-12 seeds - first v
-    //Need 32 with 0 wins from 5-16 seeds
-    placeTeam 2 "Champ"
-    placeTeam 2 "RunnerUp"
-    placeTeam 6 "FinalFour"
-    placeTeam 6 "FinalFour"
-    placeTeam 6 "Elite 8"
-    placeTeam 6 "Elite 8"
-    placeTeam 6 "Elite 8"
-    placeTeam 6 "Elite 8"
-    placeTeam 12 "SweeetSixteen"
-    placeTeam 12 "SweeetSixteen"
-    placeTeam 12 "SweeetSixteen"
-    placeTeam 12 "SweeetSixteen"
-    placeTeam 12 "SweeetSixteen"
-    placeTeam 12 "SweeetSixteen"
-    placeTeam 12 "SweeetSixteen"
-    placeTeam 12 "SweeetSixteen"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-    placeTeam 12 "FirstWin"
-
-    teamPredictions 
-    |> Seq.iter(fun tp -> printfn "%s:%s" tp.Team.Id tp.Place)
+teamPredictions |> Seq.iter(fun tp -> printfn "%s:%s" tp.Team.Id tp.Place);;
